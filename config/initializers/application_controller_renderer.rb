dd6d4307b82e7be3744712096e619543c9a21d48
東峰いくみ <ikumitomine@touhouiuminombp.lan>
2020-08-27 17:07:32 +0900

    [Add]5-6プロフィール編集機能追加

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '7dedaef6a600a303ca7fe73016a83130b6c3d081a00cc3fea79e50a4c0eb96199f26df76b9f09748ade1957df6a82bc4a73235cc0971b63ea3e13463bd20a781'
